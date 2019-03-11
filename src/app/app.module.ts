4cbfbabbe6136f7d104a50a836bb7f7ce3ffd2cc
hamna-inginim <hamna.o@inginim.com>
2019-03-11 17:32:43 +0530

    master brach cleaned

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,25 +1,16 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-import {BrowserAnimationsModule} from '@angular/platform-browser/animations';
-import {MatSidenavModule, MatIconModule, MatToolbarModule, MatListModule} from '@angular/material';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-import { LayoutComponent } from './components/layout/layout.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    LayoutComponent,
 
   ],
   imports: [
     BrowserModule,
     AppRoutingModule,
-    BrowserAnimationsModule,
-    MatSidenavModule,
-    MatIconModule,
-    MatToolbarModule,
-    MatListModule
   ],
   providers: [],
   bootstrap: [AppComponent]
